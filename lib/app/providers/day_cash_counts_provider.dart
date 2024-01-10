42dce5e29b6245529deb9071bfefacac433d8f97
Jose Carlos <joseca20025@gmail.com>
2024-01-10 10:16:07 -0400

    Create daycashcounts provider

---
+++ lib/app/providers/day_cash_counts_provider.dart
@@ -0,0 +1,18 @@
+import 'package:arqueo_ahsc/app/models/day_cash_count.dart';
+import 'package:flutter/material.dart';
+
+class DayCashCountsProvider extends ChangeNotifier {
+  final List<DayCashCount> _dayCashCounts = [];
+
+  List<DayCashCount> get dayCashCounts => _dayCashCounts;
+
+  void addDayCashCount(DayCashCount dayCashCount) {
+    _dayCashCounts.add(dayCashCount);
+    notifyListeners();
+  }
+
+  void removeDayCashCount(DayCashCount dayCashCount) {
+    _dayCashCounts.remove(dayCashCount);
+    notifyListeners();
+  }
+}
