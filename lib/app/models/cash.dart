505c6a45ebdbbcd56e30f8ea0d58ea2ebdf1a8d3
Jose Carlos <joseca20025@gmail.com>
2024-01-10 14:09:14 -0400

    Create model Cash

---
+++ lib/app/models/cash.dart
@@ -0,0 +1,13 @@
+class Cash {
+  Cash({
+    required this.id,
+    required this.name,
+    required this.amount,
+    required this.total,
+  });
+
+  final String id;
+  final String name;
+  final int amount;
+  final double total;
+}
