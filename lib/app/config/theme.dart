3e3a97a190a8a0ad5dbd508df307b9443c47f97b
Jose Carlos <joseca20025@gmail.com>
2024-01-10 09:31:44 -0400

    Delete dark theme

--- lib/app/config/theme.dart
+++ lib/app/config/theme.dart
@@ -2,15 +2,23 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class ThemeConfig {
-  static ThemeData getLightTheme() => ThemeData(
-        brightness: Brightness.light,
-        colorSchemeSeed: Colors.blue,
-        fontFamily: GoogleFonts.poppins().fontFamily,
-      );
+  static const primaryColor = Color(0xFFF5DF99);
+  static const secondaryColor = Color(0xFF5FD068);
+  static const backgroundColor = Color(0xFFF6FBF4);
 
-  static ThemeData getDarkTheme() => ThemeData(
-        brightness: Brightness.dark,
-        colorSchemeSeed: Colors.blue,
+  static ThemeData getTheme() => ThemeData(
+        brightness: Brightness.light,
+        colorSchemeSeed: primaryColor,
         fontFamily: GoogleFonts.poppins().fontFamily,
+
+        // Scaffold background color
+        scaffoldBackgroundColor: backgroundColor,
+
+        // Appbar Theme
+        appBarTheme: const AppBarTheme(
+          backgroundColor: backgroundColor,
+          elevation: 0,
+          iconTheme: IconThemeData(color: Colors.black),
+        ),
       );
 }
