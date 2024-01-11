a3fcd4876f5e00936fda274fcf14d102ec13c90b
Jose Carlos <joseca20025@gmail.com>
2024-01-11 18:04:31 -0400

    Add SnackBar to avoid creating a new dayCashCount before closing the current one

--- lib/app/pages/home_page.dart
+++ lib/app/pages/home_page.dart
@@ -1,13 +1,19 @@
+import 'package:arqueo_ahsc/app/helpers/build_error_snack_bar.dart';
 import 'package:arqueo_ahsc/app/pages/add_cash_count_page.dart';
+import 'package:arqueo_ahsc/app/providers/day_cash_counts_provider.dart';
 import 'package:arqueo_ahsc/app/widgets/ahsc_logo.dart';
 import 'package:arqueo_ahsc/app/widgets/day_cash_counts_list.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final DayCashCountsProvider dayCashCountsProvider =
+        context.read<DayCashCountsProvider>();
+
     return Scaffold(
         //* AppBar
         appBar: AppBar(
@@ -23,6 +29,14 @@ class HomePage extends StatelessWidget {
         //* Floating Action Button
         floatingActionButton: FloatingActionButton(
           onPressed: () {
+            if (!dayCashCountsProvider.dayCashCounts.first.isClosed) {
+              showErrorSnackBar(context,
+                  title:
+                      'Debes cerrar el arqueo actual antes de agregar uno nuevo');
+
+              return;
+            }
+
             Navigator.of(context).push(
               MaterialPageRoute(
                 builder: (_) => const AddCashCountPage(),
