f62ca19449984eda752fa6cda29a6e6e6946b189
Jose Carlos <joseca20025@gmail.com>
2024-01-13 15:46:34 -0400

    Add incomes provider in app

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -2,6 +2,7 @@ import 'package:arqueo_ahsc/app/config/theme.dart';
 import 'package:arqueo_ahsc/app/pages/home_page.dart';
 import 'package:arqueo_ahsc/app/pages/incomes_page.dart';
 import 'package:arqueo_ahsc/app/providers/day_cash_counts_provider.dart';
+import 'package:arqueo_ahsc/app/providers/incomes_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -14,6 +15,7 @@ class App extends StatelessWidget {
       providers: [
         // Aquí van los providers
         ChangeNotifierProvider(create: (_) => DayCashCountsProvider()),
+        ChangeNotifierProvider(create: (_) => IncomesProvider()),
       ],
       child: MaterialApp(
         title: 'Arqueo AHSC',
