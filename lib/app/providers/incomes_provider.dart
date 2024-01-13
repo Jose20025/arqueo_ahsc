0472df5b8cf69e3871d1b88670a102ba3b833ba2
Jose Carlos <joseca20025@gmail.com>
2024-01-13 15:48:12 -0400

    Update incomes provider to do some other functions

--- lib/app/providers/incomes_provider.dart
+++ lib/app/providers/incomes_provider.dart
@@ -2,12 +2,28 @@ import 'package:arqueo_ahsc/app/models/income.dart';
 import 'package:flutter/material.dart';
 
 class IncomesProvider extends ChangeNotifier {
-  final List<Income> _incomes = [];
+  final List<Income> _incomes = [
+    Income(id: '1', amount: 200.32, description: 'Venta de pan'),
+  ];
 
   List<Income> get incomes => _incomes;
 
   void addIncome(Income income) {
     _incomes.add(income);
+
+    notifyListeners();
+  }
+
+  void removeIncome(String id) {
+    _incomes.removeWhere((income) => income.id == id);
+
+    notifyListeners();
+  }
+
+  void updateIncomeAmount(String id, double amount) {
+    final index = _incomes.indexWhere((element) => element.id == id);
+
+    _incomes[index].amount = amount;
 
     notifyListeners();
   }
