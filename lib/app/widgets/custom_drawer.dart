c7d180ff6802f772d18855ab189b272d2a67071b
Jose Carlos <joseca20025@gmail.com>
2024-01-13 11:38:17 -0400

    Add SafeArea to Drawer

--- lib/app/widgets/custom_drawer.dart
+++ lib/app/widgets/custom_drawer.dart
@@ -11,50 +11,52 @@ class CustomDrawer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Drawer(
-      child: Padding(
-        padding: const EdgeInsets.all(10),
-        child: ListView(
-          children: [
-            const DrawerHeader(
-              child: AHSCLogo(height: 100),
-            ),
-            _CustomDrawerNavButton(
-              title: 'Inicio',
-              icon: Icons.home,
-              activePage: page,
-              pageToCompare: Active.home,
-              onPressed: () {
-                if (page == Active.home) return;
+    return SafeArea(
+      child: Drawer(
+        child: Padding(
+          padding: const EdgeInsets.all(10),
+          child: ListView(
+            children: [
+              const DrawerHeader(
+                child: AHSCLogo(height: 100),
+              ),
+              _CustomDrawerNavButton(
+                title: 'Inicio',
+                icon: Icons.home,
+                activePage: page,
+                pageToCompare: Active.home,
+                onPressed: () {
+                  if (page == Active.home) return;
 
-                Navigator.pushReplacementNamed(context, '/');
-              },
-            ),
-            const SizedBox(height: 5),
-            _CustomDrawerNavButton(
-              title: 'Ingresos',
-              icon: Icons.wallet,
-              activePage: page,
-              onPressed: () {
-                if (page == Active.incomes) return;
+                  Navigator.pushReplacementNamed(context, '/');
+                },
+              ),
+              const SizedBox(height: 5),
+              _CustomDrawerNavButton(
+                title: 'Ingresos',
+                icon: Icons.wallet,
+                activePage: page,
+                onPressed: () {
+                  if (page == Active.incomes) return;
 
-                Navigator.pushReplacementNamed(context, '/incomes');
-              },
-              pageToCompare: Active.incomes,
-            ),
-            const SizedBox(height: 5),
-            _CustomDrawerNavButton(
-              title: 'Gastos',
-              icon: Icons.account_balance_wallet_outlined,
-              activePage: page,
-              pageToCompare: Active.expenses,
-              onPressed: () {
-                if (page == Active.expenses) return;
+                  Navigator.pushReplacementNamed(context, '/incomes');
+                },
+                pageToCompare: Active.incomes,
+              ),
+              const SizedBox(height: 5),
+              _CustomDrawerNavButton(
+                title: 'Gastos',
+                icon: Icons.account_balance_wallet_outlined,
+                activePage: page,
+                pageToCompare: Active.expenses,
+                onPressed: () {
+                  if (page == Active.expenses) return;
 
-                Navigator.pushReplacementNamed(context, '/expenses');
-              },
-            )
-          ],
+                  Navigator.pushReplacementNamed(context, '/expenses');
+                },
+              )
+            ],
+          ),
         ),
       ),
     );
