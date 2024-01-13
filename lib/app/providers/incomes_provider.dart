345f173f06cff2632c91a0692e5879d6478069c5
Jose Carlos <joseca20025@gmail.com>
2024-01-13 18:50:37 -0400

    Add expenses and incomes providers, and load data on home page initialization

--- lib/app/providers/incomes_provider.dart
+++ lib/app/providers/incomes_provider.dart
@@ -1,19 +1,52 @@
+import 'dart:convert';
+
 import 'package:arqueo_ahsc/app/models/income.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class IncomesProvider extends ChangeNotifier {
   final List<Income> _incomes = [];
 
   List<Income> get incomes => _incomes;
 
+  void loadIncomes() async {
+    final sharedPrefs = await SharedPreferences.getInstance();
+
+    final List<String> incomesString =
+        sharedPrefs.getStringList('incomes') ?? [];
+
+    _incomes.clear();
+
+    for (var incomeString in incomesString) {
+      final Income income = Income.fromJson(jsonDecode(incomeString));
+
+      _incomes.add(income);
+    }
+
+    notifyListeners();
+  }
+
+  void saveIncomes() async {
+    final sharedPrefs = await SharedPreferences.getInstance();
+
+    final List<String> incomesString =
+        _incomes.map((income) => jsonEncode(income.toJson())).toList();
+
+    sharedPrefs.setStringList('incomes', incomesString);
+  }
+
   void addIncome(Income income) {
     _incomes.add(income);
+
+    saveIncomes();
 
     notifyListeners();
   }
 
   void removeIncome(String id) {
     _incomes.removeWhere((income) => income.id == id);
+
+    saveIncomes();
 
     notifyListeners();
   }
@@ -22,6 +55,8 @@ class IncomesProvider extends ChangeNotifier {
     final index = _incomes.indexWhere((income) => income.id == id);
 
     _incomes[index] = newIncome;
+
+    saveIncomes();
 
     notifyListeners();
   }
