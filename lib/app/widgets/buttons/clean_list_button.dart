077e0deb36d9996f797bf01e9e4c84ac6dfbe274
Jose Carlos <joseca20025@gmail.com>
2024-01-23 20:40:56 -0400

    Refactor CleanListButton widget constructor

--- lib/app/widgets/buttons/clean_list_button.dart
+++ lib/app/widgets/buttons/clean_list_button.dart
@@ -1,18 +1,26 @@
 import 'package:flutter/material.dart';
 
 class CleanListButton extends StatelessWidget {
-  const CleanListButton({super.key, required this.onPressed});
+  const CleanListButton({
+    super.key,
+    required this.onPressed,
+    this.color,
+    this.iconColor,
+  });
 
   final void Function() onPressed;
+  final Color? color;
+  final Color? iconColor;
 
   @override
   Widget build(BuildContext context) {
     return IconButton.filledTonal(
       onPressed: onPressed,
-      color: Colors.white,
-      style: const ButtonStyle(
-        backgroundColor: MaterialStatePropertyAll(Colors.blue),
-        shape: MaterialStatePropertyAll(
+      color: iconColor,
+      style: ButtonStyle(
+        backgroundColor:
+            color != null ? const MaterialStatePropertyAll(Colors.blue) : null,
+        shape: const MaterialStatePropertyAll(
           RoundedRectangleBorder(
             borderRadius: BorderRadius.all(Radius.circular(10)),
           ),
